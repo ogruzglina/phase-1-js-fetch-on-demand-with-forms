23bfe5d116f9bb6f2d64deea012d80f8f4945cd4
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-19 15:19:03 -0500

    commit tree 0efce23a77e7b623bc6b4df9aec3fb3daaaf1ede

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 const init = () => {
-  
+    const form = document.querySelector('section form');
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const inputValue = event.target.children[1].value;
+// or - const inputValue = document.getElementById('searchByID').value;
+
+        event.target.reset();
+
+        fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then(resp => resp.json())
+        .then(data => showFindResult(data));
+
+        // when we try to find element by code not with URL
+ //    fetch(`http://localhost:3000/movies`)
+//     .then(resp => resp.json())
+//     .then(data => findElement(inputValue, data);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function showFindResult(data) {
+    const title = document.querySelector('#movieDetails h4');
+    const summary = document.querySelector('#movieDetails p');
+
+    title.textContent = data.title;
+    summary.textContent = data.summary;
+}
+
+// function findElement(inputValue, arrOfData) {
+            // arrOfData.forEach(element => {
+            //     if(element.id === parseInt(inputValue)) {
+            //         showFindResult(element);
+//              }
+//          })
+// }
